9d286b029593075087edd26c99b112a191d3b76d
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-01-27 22:23:43 -0500

    analytics

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded',function(){
 	var backButton = document.getElementById("back")
 	if(backButton) backButton.onclick = function(){ window.history.back(); return false; }
+
+	/* Analytics */
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-43866715-1']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
+		
 })
