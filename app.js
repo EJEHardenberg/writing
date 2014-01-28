65f64b8244a23c6cb47c79f010a1ed3178d06d27
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-01-27 21:20:54 -0500

    Added index pages.
    Added index pages, back button uses history to go back. Need to add more
    content into the site.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded',function(){
-	/* Poetry Loading */
-	
+	var backButton = document.getElementById("back")
+	if(backButton) backButton.onclick = function(){ window.history.back(); return false; }
 })
